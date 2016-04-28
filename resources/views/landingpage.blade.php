071144b5399dc17b0d271b5f84e70ad355da5905
Stefan Zweifel <hello@stefanzweifel.io>
2016-04-28 21:35:08 +0200

    Add Screenshot
    See #2
    
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -14,38 +14,39 @@
         </p>
 
         <div class="mv3">
-            <a href="/" target="blank">
-                <img src="//placehold.it/200x150" class="di w-50 mw-100" alt="">
+
+            <a href="/images/screenshot-of-report.png" target="blank" title="Screenshot of a finished Report">
+                <img src="/images/screenshot-of-report.png" class="di w-75 mw-100" alt="">
             </a>
+
+            @if (Session::has('success'))
+
+                <section class="bg-green white ba b--black-10 mv4 mw-100">
+                    <div class="pv2 ph3">
+                        <p class="ma0">We sent an email with an upload link to you.</p>
+                    </div>
+                </section>
+
+            @else
+
+                {!! Form::open(['route' => 'process.store']) !!}
+
+                    {!! Form::email('email', '', [
+                        'class' => 'input-reset ba mv2 b--gray pa2 w-100 ',
+                        'placeholder' => 'Your email to start the process!'
+                    ]) !!}
+                    <button type="submit" class="btn btn--black f6 pv2 ph3 br1 b--near-black">Let's do this!</button>
+
+                {!! Form::close() !!}
+
+            @endif
+
+            <p class="lh-copy f6">
+                To prevent abuse of the system you have to provide a valid email-adress.
+                We will send you a unique link to upload your file.
+            </p>
+
         </div>
-
-
-        @if (Session::has('success'))
-
-            <section class="bg-green white ba b--black-10 mv4 mw-100">
-                <div class="pv2 ph3">
-                    <p class="ma0">We sent an email with an upload link to you.</p>
-                </div>
-            </section>
-
-        @else
-
-            {!! Form::open(['route' => 'process.store']) !!}
-
-                {!! Form::email('email', '', [
-                    'class' => 'input-reset ba mv2 b--gray pa2 w-100 ',
-                    'placeholder' => 'Your email to start the process!'
-                ]) !!}
-                <button type="submit" class="btn btn--black f6 pv2 ph3 br1 b--near-black">Let's do this!</button>
-
-            {!! Form::close() !!}
-
-        @endif
-
-        <p class="lh-copy f6">
-            To prevent abuse of the system you have to provide a valid email-adress.
-            We will send you a unique link to upload your file.
-        </p>
 
     </div>
 
